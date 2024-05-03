e6f9335bf22c184c90caa1e874e6fc278371a5fa
youssef Maimouni <youssefmaimouni03@gmail.com>
2024-05-03 20:26:43 +0100

    stack navigator s1 s2

--- interface/PV.js
+++ interface/PV.js
@@ -1,95 +1,19 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { Rapp, Scanner, Signature ,Acceuil,Etudiants} from './navigation/indexe';
-import {Entypo , MaterialCommunityIcons,FontAwesome5 ,FontAwesome ,Fontisto} from '@expo/vector-icons';
-import { View,Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Seance1 from './seance1';
+import Seance2 from './seance2';
 
 
-const Tab = createBottomTabNavigator();
-const screenOptions={
-    tabBarShowLabel:false,
-    headerShown:true,
-       tabBarStyle:{
-           position:'absolute',
-           height:60,
-           bottom:0,
-           right:0,
-           left:0,
-           elevation:0,
-           borderBottomLeftRadius:16,
-           background:'#fff',
-           borderTopWidth:1,
-       }
-}
+const Stack=createNativeStackNavigator();
+
 
 export default function PV() {
   return (
     <NavigationContainer>
-         <Tab.Navigator screenOptions={screenOptions}>
-      
-          
-         <Tab.Screen name='Rapport' component={Rapp} options={{
-            tabBarIcon:({focused})=>{
-                return(
-                    
-                    <View style={{alignItems:"center" ,justifyContent:"center" }} >
-                    <FontAwesome name="file-text-o" size={24} color="black" />               
-                    <Text>Rapp</Text>
-                </View>
-                )
-            }
-        }}/>
-        <Tab.Screen name='Scanner' component={Scanner} options={{
-            tabBarIcon:({focused})=>{
-              return(
-
-                <View style={{alignItems:"center" ,justifyContent:"center" }} >
-                    <MaterialCommunityIcons name="qrcode-scan" size={24} color="black" />
-                    <Text>Scanner</Text>
-                </View>
-                )
-            }
-        }}/>
-        <Tab.Screen name='Acceuil'
-       component={Acceuil}
-       options={{
-           tabBarIcon:({focused})=>{
-             return(
-
-               <View style={{alignItems:"center" ,justifyContent:"center" }} >
-                   <Entypo name='home' size={24} color="black" />
-                   <Text>Acceuil</Text>
-               </View>
-               )
-           }
-       }}
-        />
-             <Tab.Screen name='Etudiants' component={Etudiants} options={{
-                  tabBarIcon:({focused})=>{
-                      return(
-                          
-                          <View style={{alignItems:"center" ,justifyContent:"center" }} >
-                        <Fontisto name="persons" size={24} color="black" />
-                         <Text>Etudiants</Text>
-                     </View>
-                     )
-                 }
-             }} />
-         <Tab.Screen name='Signature' component={Signature} options={{
-            tabBarIcon:({focused})=>{
-              return(
-
-                <View style={{alignItems:"center" ,justifyContent:"center" }} >
-                    <FontAwesome5 name="signature" size={24} color="black" />
-                    <Text>Signature</Text>
-                </View>
-                )
-            }
-        }}/>
-         
-      
-
-     </Tab.Navigator> 
+         <Stack.Navigator initialRouteName='Seance1'>
+            <Stack.Screen name="Seance1" component={Seance1} />
+            <Stack.Screen name="Seance2" component={Seance2} />
+         </Stack.Navigator>
     </NavigationContainer>
     
   );
---
+++ interface/seance2.js
@@ -0,0 +1,111 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Rapp, Scanner, Signature ,Acceuil,Etudiants} from './navigation/indexe';
+import {Entypo , MaterialCommunityIcons,FontAwesome5 ,FontAwesome ,Fontisto} from '@expo/vector-icons';
+import { View,Text,StyleSheet,Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+const Tab = createBottomTabNavigator();
+const screenOptions={
+    tabBarShowLabel:false,
+    headerShown:false,
+       tabBarStyle:{
+           position:'absolute',
+           height:60,
+           bottom:0,
+           right:0,
+           left:0,
+           elevation:0,
+           borderBottomLeftRadius:16,
+           background:'#fff',
+           borderTopWidth:1,
+       }
+}
+
+export default function Seance2() {
+    const navigation=useNavigation();
+  return (<View style={styles.page}>
+    <View style={styles.container}>
+            <View style={styles.button} ><Button title='Seance 1' onPress={()=>navigation.navigate("Seance1")} /></View>
+            <View style={styles.button}><Button title='Seance 2' onPress={()=>navigation.navigate("Seance2")} /></View>
+     </View>
+        <Tab.Navigator screenOptions={screenOptions} initialRouteName='Acceuil' >
+         <Tab.Screen name='Rapport' component={Rapp} options={{
+            tabBarIcon:({focused})=>{
+                return(
+                    
+                    <View style={{alignItems:"center" ,justifyContent:"center" }} >
+                    <FontAwesome name="file-text-o" size={24} color="black" />               
+                    <Text>Rapp</Text>
+                </View>
+                )
+            }
+        }}/>
+        <Tab.Screen name='Scanner' component={Scanner} options={{
+            tabBarIcon:({focused})=>{
+              return(
+
+                <View style={{alignItems:"center" ,justifyContent:"center" }} >
+                    <MaterialCommunityIcons name="qrcode-scan" size={24} color="black" />
+                    <Text>Scanner</Text>
+                </View>
+                )
+            }
+        }}/>
+        <Tab.Screen name='Acceuil'
+       component={Acceuil}
+       options={{
+           tabBarIcon:({focused})=>{
+             return(
+
+               <View style={{alignItems:"center" ,justifyContent:"center" }} >
+                   <Entypo name='home' size={24} color="black" />
+                   <Text>Acceuil</Text>
+               </View>
+               )
+           }
+       }}
+        />
+             <Tab.Screen name='Etudiants' component={Etudiants} options={{
+                  tabBarIcon:({focused})=>{
+                      return(
+                          
+                          <View style={{alignItems:"center" ,justifyContent:"center" }} >
+                        <Fontisto name="persons" size={24} color="black" />
+                         <Text>Etudiants</Text>
+                     </View>
+                     )
+                 }
+             }} />
+         <Tab.Screen name='Signature' component={Signature} options={{
+            tabBarIcon:({focused})=>{
+              return(
+
+                <View style={{alignItems:"center" ,justifyContent:"center" }} >
+                    <FontAwesome5 name="signature" size={24} color="black" />
+                    <Text>Signature</Text>
+                </View>
+                )
+            }
+        }}/>
+         
+      
+
+     </Tab.Navigator> 
+   
+    </View>
+  );
+}
+
+const styles=StyleSheet.create({
+    page:{
+        height:600
+    },
+    button:{
+        width:210,
+    },
+    container:{
+        flexDirection:'row'
+    }
+})
